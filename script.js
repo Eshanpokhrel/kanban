83ef1cdac8aea58afab833512ced5fb1748ea037
Eshanpokhrel <eshanpokhrel12@gmail.com>
2023-10-03 21:43:43 +0545

    todo added

---
+++ script.js
@@ -0,0 +1,26 @@
+const addTask = document.getElementById("addTask");
+const input = document.getElementById("todoInput");
+const todo = document.getElementById("toDo");
+const addBtn = document.getElementById("add");
+
+addBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const value = input.value;
+
+    if (!value) return;
+
+    const newTask = document.createElement("p");
+    newTask.classList.add("task")
+    newTask.setAttribute("draggable","true")
+    newTask.innerText = value;
+
+    newTask.addEventListener("dragstart",()=>{
+        newTask.classList.add("isDragging")
+    });
+    newTask.addEventListener("dragend",()=>{
+        newTask.classList.remove("isDragging")
+    });
+
+    todo.appendChild(newTask);
+    input.value = "";    
+});
